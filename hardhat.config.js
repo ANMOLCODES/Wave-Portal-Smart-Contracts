394da2e686e51a060447def767285df1d934e58a
Anmol Arora <mr.anmolarora@outlook.com>
2022-02-28 23:58:45 +0530

    Smart contracts

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/nuNkmPgJ1WD9zg3KRERhE4zR5EuYcmJr",
+      accounts: ["881280e8f89ba0eff735101ef95b6b8e3caf3bc1f6583a9ec4f5985ac2f694e9"]
+    },
+  },
+};
